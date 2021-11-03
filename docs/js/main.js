587b338d84ca0818576a810d7840e88e0c3d7b93
Aadhi <aadhi@pop-os.localdomain>
2021-11-03 12:00:05 +0300

    modified input field

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,8 +2,11 @@ let message = "Hello, World";
 console.log(message);
 
 function respond_to_hello() {
-    let input = document.getElementById("ccard-input").value;
+    let input = document.getElementById("cmd").value;
     if(input == "hello") {
-        document.getElementById("output").innerHTML = "bye";
+        console.log("hello");
+    }
+    if(input == "bye") {
+        console.log("bye");
     }
 }
