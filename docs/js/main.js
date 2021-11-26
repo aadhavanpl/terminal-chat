9cf64464a666d5de67e06772ff87ae045931d314
Aadhi <aadhi@pop-os.localdomain>
2021-11-26 20:09:24 +0300

    changed input area

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,12 +1,26 @@
 let message = "Hello, World";
 console.log(message);
 
-function respond_to_hello() {
+function respond() {
     let input = document.getElementById("cmd").value;
-    if(input == "hello") {
+    if(input =="help") {
+        help();
+    }
+    else if(input == "hello") {
         document.getElementById("output").innerHTML = "hello";
     }
-    if(input == "bye") {
+    else if(input == "bye") {
         document.getElementById("output").innerHTML = "bye";
     }
+    else {
+        document.getElementById("output").innerHTML = "command not found";
+    }
+    document.getElementById("cmd").value = ""; //to clear value in input 
+}
+
+function help() {
+    var commands = `hello\n
+    bye`;
+    document.getElementById("output").innerHTML = commands;
+    
 }
